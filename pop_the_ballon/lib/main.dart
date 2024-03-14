5d1f4e97246e9730e6095de847c9203e46bd90c6
vigneshkarthee <vikkikarthee@gmail.com>
2024-03-14 11:29:45 +0530

    added

--- pop_the_ballon/lib/main.dart
+++ pop_the_ballon/lib/main.dart
@@ -1,151 +1,125 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:math';
 
 void main() {
-  runApp(BalloonPopGame());
+  runApp(const MyApp());
 }
 
-class BalloonPopGame extends StatelessWidget {
-  const BalloonPopGame({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Balloon Pop Game',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: GameScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class GameScreen extends StatefulWidget {
-  const GameScreen({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  _GameScreenState createState() => _GameScreenState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _GameScreenState extends State<GameScreen> {
-  late Timer timer;
-  int score = 0;
-  int balloonsPopped = 0;
-  int balloonsMissed = 0;
-  bool isGameOver = false;
-  List<Offset> balloons = [];
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    startGame();
-  }
-
-  void startGame() {
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (timer.tick == 120) {
-          endGame();
-        }
-      });
-    });
-    generateBalloons();
-  }
-
-  void endGame() {
-    timer.cancel();
+  void _incrementCounter() {
     setState(() {
-      isGameOver = true;
-    });
-  }
-
-  void generateBalloons() {
-    Random random = Random();
-    timer = Timer.periodic(Duration(milliseconds: 1000), (timer) {
-      setState(() {
-        balloons.add(Offset(random.nextDouble() * 300, 600));
-      });
-    });
-  }
-
-  void popBalloon(Offset position) {
-    setState(() {
-      balloons.remove(position);
-      score += 2;
-      balloonsPopped++;
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text('Balloon Pop Game'),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Time: ${120 - timer.tick ~/ 60}:${(120 - timer.tick % 60).toString().padLeft(2, '0')}',
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Score: $score',
-                style: TextStyle(fontSize: 24),
-              ),
-            ],
-          ),
-          ...balloons.map((balloon) {
-            return Positioned(
-              left: balloon.dx,
-              top: balloon.dy,
-              child: GestureDetector(
-                onTap: () => popBalloon(balloon),
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-              ),
-            );
-          }),
-          if (isGameOver)
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Game Over!',
-                    style: TextStyle(fontSize: 36),
-                  ),
-                  Text(
-                    'Score: $score',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        score = 0;
-                        balloonsPopped = 0;
-                        balloonsMissed = 0;
-                        isGameOver = false;
-                      });
-                      startGame();
-                    },
-                    child: Text('Play Again'),
-                  ),
-                ],
-              ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-        ],
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
